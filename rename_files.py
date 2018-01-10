c4a3aba2b26182298c129cd831bf7db0bed0325f
boscoybarra <bosco@ybarra.io>
2018-01-10 23:25:06 +0100

    added new text

--- rename_files.py
+++ rename_files.py
@@ -3,7 +3,7 @@ import os
 def rename_files():
     # 1 get file name from a folder
     file_list = os.listdir("/Users/jb/Desktop/prank/")
-    
+
     # print(file_list)
 
     # 1.1 Check our current working directory
@@ -18,8 +18,8 @@ def rename_files():
         translation_table = str.maketrans("0123456789", "          ", "0123456789")
 
         # Print in the console the New and Old Names
-        print("Old Name - " +file_name)
-        print("New Name - " +file_name.translate(translation_table))
+        print("Old Name File - " +file_name)
+        print("New Name File - " +file_name.translate(translation_table))
         os.rename(file_name, file_name.translate(translation_table))
     # We change the path back to where we find it before applying chdir
     os.chdir(saved_path)
