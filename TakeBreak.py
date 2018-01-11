44763532707d26f61691e1cd8e58996580b18946
boscoybarra <bosco@ybarra.io>
2018-01-11 22:50:14 +0100

    working on take a break app

--- TakeBreak.py
+++ TakeBreak.py
@@ -6,9 +6,9 @@ break_count = 0
 
 print("This program started on "+time.ctime())
 
+
+# This while loop runs currently every 2 hours
 while (break_count < total_breaks):
     time.sleep(2*60*60)
     webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
     break_count =+ 1
-
-
